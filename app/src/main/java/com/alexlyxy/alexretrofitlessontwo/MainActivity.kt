9282d0465136a3814db809081e6bc5ef875fd77d
Oleksandr Lysenko <alexlyxy@gmail.com>
2023-10-19 00:18:25 +0300

    Initial commit

---
+++ app/src/main/java/com/alexlyxy/alexretrofitlessontwo/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.alexlyxy.alexretrofitlessontwo
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val tv = findViewById<TextView>(R.id.tv)
+        val b = findViewById<Button>(R.id.button)
+
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://dummyjson.com").client(client)
+            .addConverterFactory(GsonConverterFactory.create()).build()
+        val productApi = retrofit.create(ProductApi::class.java)
+
+        b.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                val product = productApi.getProductById(3)
+                runOnUiThread {
+                    tv.text = product.title
+                }
+            }
+        }
+    }
+}
