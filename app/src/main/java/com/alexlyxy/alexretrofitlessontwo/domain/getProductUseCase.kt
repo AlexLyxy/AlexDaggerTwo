ed1c7fa3eaa8fa8ffa255db6bd1986f75df66e28
Oleksandr Lysenko <alexlyxy@gmail.com>
2024-03-14 22:09:15 +0200

    Full Product isSuccesful 14.03.2024

--- app/src/main/java/com/alexlyxy/alexretrofitlessontwo/domain/getProductUseCase.kt
+++ app/src/main/java/com/alexlyxy/alexretrofitlessontwo/domain/getProductUseCase.kt
@@ -6,17 +6,17 @@ import com.alexlyxy.alexretrofitlessontwo.data.ProductApi
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class GetProductUseCase {
-
-    private val retrofit = Retrofit.Builder()
-        .baseUrl(Constants.BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create()).build()
-    private val productApi: ProductApi = retrofit.create(ProductApi::class.java)
-
-    suspend fun getLatestProduct(): Product {
-        return productApi.getProduct(9)
-    }
-}
+//class GetProductUseCase {
+//
+//    private val retrofit = Retrofit.Builder()
+//        .baseUrl(Constants.BASE_URL)
+//        .addConverterFactory(GsonConverterFactory.create()).build()
+//    private val productApi: ProductApi = retrofit.create(ProductApi::class.java)
+//
+//    suspend fun getLatestProduct(): Product {
+//        return productApi.getProduct(9)
+//    }
+//}
 
 
 
--- app/src/main/java/com/alexlyxy/alexretrofitlessontwo/presentation/MainActivity.kt
+++ app/src/main/java/com/alexlyxy/alexretrofitlessontwo/presentation/MainActivity.kt
@@ -2,16 +2,11 @@ package com.alexlyxy.alexretrofitlessontwo.presentation
 
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.alexlyxy.alexretrofitlessontwo.Constants
+import com.alexlyxy.alexretrofitlessontwo.data.ProductApi
 import com.alexlyxy.alexretrofitlessontwo.databinding.ActivityMainBinding
-import com.alexlyxy.alexretrofitlessontwo.domain.GetProductUseCase
+import com.alexlyxy.alexretrofitlessontwo.presentation.dialogs.ServerErrorDialogFragment
 import com.squareup.picasso.Picasso
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -20,23 +15,37 @@ import kotlinx.coroutines.cancelChildren
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.coroutines.cancellation.CancellationException
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var  getProductUseCase:  GetProductUseCase
+    //private lateinit var  getProductUseCase:  GetProductUseCase
+
+    private val coroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
 
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var productApi: ProductApi
+
+    private var isDataLoaded = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        getProductUseCase = GetProductUseCase ()
+        //getProductUseCase = GetProductUseCase ()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        productApi = retrofit.create(ProductApi::class.java)
 
         binding.button.setOnClickListener {
             CoroutineScope(Dispatchers.IO).launch {
-                val product = getProductUseCase.getLatestProduct()
+                //val product = getProductUseCase.getLatestProduct()
+                val product = productApi.getProduct(8)
 
                 Log.d("MyLog", "Product : $product")
 
@@ -46,92 +55,53 @@ class MainActivity : AppCompatActivity() {
 
                         tvTitle.text = buildString {
                             append("Title:  ")
-                            append(product.title)
+                            append(product.body()?.title)
                         }
                         tvDescr.text = buildString {
                             append("Description:  ")
-                            append(product.description)
+                            append(product.body()?.description)
                         }
                         tvPrice.text = buildString {
                             append("Price:  ")
-                            append(product.price.toString())
+                            append(product.body()?.price)
                         }
                         tvDiscount.text = buildString {
                             append("DiscountPercentage:  ")
-                            append(product.discountPercentage.toString())
+                            append(product.body()?.discountPercentage)
                         }
                         tvRating.text = buildString {
                             append("Rating:  ")
-                            append(product.rating.toString())
+                            append(product.body()?.rating)
                         }
                         tvStock.text = buildString {
                             append("Stock:  ")
-                            append(product.stock.toString())
+                            append(product.body()?.stock)
                         }
                         tvBrand.text = buildString {
                             append("Brand:  ")
-                            append(product.brand)
+                            append(product.body()?.brand)
                         }
                         tvCategory.text = buildString {
                             append("Category:  ")
-                            append(product.category)
+                            append(product.body()?.category)
                         }
                         tvThumbnail.text = buildString {
                             append("Thumbnail:  ")
-                            append(product.thumbnail)
+                            append(product.body()?.thumbnail)
                         }
-                        Picasso.get().load(product.images[1]).into(ivImageOne)
-                        Picasso.get().load(product.images[2]).into(ivImageTwo)
-                        Picasso.get().load(product.images[3]).into(ivImageThree)
+                        Picasso.get().load(product.body()!!.images[1]).into(ivImageOne)
+                        Picasso.get().load(product.body()!!.images[2]).into(ivImageTwo)
+                        Picasso.get().load(product.body()!!.images[3]).into(ivImageThree)
                     }
                 }
             }
         }
     }
-}
-
-
-class QuestionsListActivity : AppCompatActivity() {
-
-    private val coroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
-
-    private lateinit var swipeRefresh: SwipeRefreshLayout
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var questionsAdapter: QuestionsAdapter
-    private lateinit var stackoverflowApi: StackoverflowApi
-
-    private var isDataLoaded = false
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout_questions_list)
-
-        // init pull-down-to-refresh
-        swipeRefresh = findViewById(R.id.swipeRefresh)
-        swipeRefresh.setOnRefreshListener {
-            fetchQuestions()
-        }
-
-        // init recycler view
-        recyclerView = findViewById(R.id.recycler)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        questionsAdapter = QuestionsAdapter{ clickedQuestion ->
-            QuestionDetailsActivity.start(this, clickedQuestion.id)
-        }
-        recyclerView.adapter = questionsAdapter
-
-        // init retrofit
-        val retrofit = Retrofit.Builder()
-            .baseUrl(Constants.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-        stackoverflowApi = retrofit.create(StackoverflowApi::class.java)
-    }
 
     override fun onStart() {
         super.onStart()
         if (!isDataLoaded) {
-            fetchQuestions()
+            fetchProduct()
         }
     }
 
@@ -140,14 +110,65 @@ class QuestionsListActivity : AppCompatActivity() {
         coroutineScope.coroutineContext.cancelChildren()
     }
 
-    private fun fetchQuestions() {
+    private fun fetchProduct() {
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        productApi = retrofit.create(ProductApi::class.java)
+
         coroutineScope.launch {
-            showProgressIndication()
             try {
-                val response = stackoverflowApi.lastActiveQuestions(20)
+                val response = productApi.getProduct(2)
                 if (response.isSuccessful && response.body() != null) {
-                    questionsAdapter.bindData(response.body()!!.questions)
                     isDataLoaded = true
+                    Log.d("MyLog", "Response : ${productApi.getProduct(2)}")
+
+                        binding.apply {
+
+                            tvTitle.text = buildString {
+                                append("Title:  ")
+                                append(response.body()?.title)
+                            }
+                            tvDescr.text = buildString {
+                                append("Description:  ")
+                                append(response.body()?.description)
+                            }
+                            tvPrice.text = buildString {
+                                append("Price:  ")
+                                append(response.body()?.price)
+                            }
+                            tvDiscount.text = buildString {
+                                append("DiscountPercentage:  ")
+                                append(response.body()?.discountPercentage)
+                            }
+                            tvRating.text = buildString {
+                                append("Rating:  ")
+                                append(response.body()?.rating)
+                            }
+                            tvStock.text = buildString {
+                                append("Stock:  ")
+                                append(response.body()?.stock)
+                            }
+                            tvBrand.text = buildString {
+                                append("Brand:  ")
+                                append(response.body()?.brand)
+                            }
+                            tvCategory.text = buildString {
+                                append("Category:  ")
+                                append(response.body()?.category)
+                            }
+                            tvThumbnail.text = buildString {
+                                append("Thumbnail:  ")
+                                append(response.body()?.thumbnail)
+                            }
+                            Picasso.get().load(response.body()!!.images[1]).into(ivImageOne)
+                            Picasso.get().load(response.body()!!.images[2]).into(ivImageTwo)
+                            Picasso.get().load(response.body()!!.images[3]).into(ivImageThree)
+                        }
+
+
                 } else {
                     onFetchFailed()
                 }
@@ -156,8 +177,8 @@ class QuestionsListActivity : AppCompatActivity() {
                     onFetchFailed()
                 }
             } finally {
-                hideProgressIndication()
             }
+
         }
     }
 
@@ -167,47 +188,8 @@ class QuestionsListActivity : AppCompatActivity() {
             .commitAllowingStateLoss()
     }
 
-    private fun showProgressIndication() {
-        swipeRefresh.isRefreshing = true
-    }
-
-    private fun hideProgressIndication() {
-        if (swipeRefresh.isRefreshing) {
-            swipeRefresh.isRefreshing = false
-        }
-    }
-
-    class QuestionsAdapter(
-        private val onQuestionClickListener: (Question) -> Unit
-    ) : RecyclerView.Adapter<QuestionViewHolder>() {
-
-        private var questionsList: List<Question> = java.util.ArrayList(0)
-
-        inner class QuestionViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-            val title: TextView = view.findViewById(R.id.txt_title)
-        }
-
-        fun bindData(questions: List<Question>) {
-            questionsList = ArrayList(questions)
-            notifyDataSetChanged()
-        }
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuestionViewHolder {
-            val itemView = LayoutInflater.from(parent.context)
-                .inflate(R.layout.layout_question_list_item, parent, false)
-            return QuestionViewHolder(itemView)
-        }
-
-        override fun onBindViewHolder(holder: QuestionViewHolder, position: Int) {
-            holder.title.text = questionsList[position].title
-            holder.itemView.setOnClickListener {
-                onQuestionClickListener.invoke(questionsList[position])
-            }
-        }
-
-        override fun getItemCount(): Int {
-            return questionsList.size
-        }
-
-    }
 }
+
+
+
+
