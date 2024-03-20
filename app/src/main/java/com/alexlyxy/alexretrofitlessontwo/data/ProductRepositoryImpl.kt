1aa39877b3d52e505d8984e05da404d1102e3c4a
Oleksandr Lysenko <alexlyxy@gmail.com>
2024-03-21 00:06:43 +0200

    Final fullProduct IsSuccessful Progress Bar 20.03.2024

--- app/src/main/java/com/alexlyxy/alexretrofitlessontwo/data/ProductRepositoryImpl.kt
+++ app/src/main/java/com/alexlyxy/alexretrofitlessontwo/data/ProductRepositoryImpl.kt
@@ -1,5 +1,39 @@
 package com.alexlyxy.alexretrofitlessontwo.data
 
+import com.alexlyxy.alexretrofitlessontwo.Constants
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class ProductRepositoryImpl {
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(Constants.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create()).build()
+    private val productApi: ProductApi = retrofit.create(ProductApi::class.java)
+
+    override suspend fun getLatestProduct(id: Int): Product {
+        return productApi.getProduct(8)
+    }
+
+    override suspend fun getLocalProduct(id: Int): Product {
+        return Product(
+            id = 2,
+            title = "Title",
+            description = "Description",
+            price = 549,
+            discountPercentage = 12.96,
+            rating = 4.69,
+            stock = 94,
+            brand = "Brand",
+            category = "Category",
+            thumbnail = "Thumb",
+            images = arrayListOf(
+                "",
+                "https://cdn.dummyjson.com/product-images/8/1.jpg",
+                "https://cdn.dummyjson.com/product-images/8/2.jpg",
+                "https://cdn.dummyjson.com/product-images/8/3.jpg"
+            )
+        )
+    }
 
 }
