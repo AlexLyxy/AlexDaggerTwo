0e28aa4e961dbc04bb00d8ef5994306c67a81097
Oleksandr Lysenko <alexlyxy@gmail.com>
2024-03-21 00:02:07 +0200

    Final fullProduct IsSuccessful Progress Bar 20.03.2024

--- app/src/main/java/com/alexlyxy/alexretrofitlessontwo/domain/ProductRepository.kt
+++ app/src/main/java/com/alexlyxy/alexretrofitlessontwo/domain/ProductRepository.kt
@@ -4,5 +4,9 @@ import com.alexlyxy.alexretrofitlessontwo.data.Product
 import retrofit2.Response
 
 interface ProductRepository {
-    suspend fun getLatestProduct(): Response<Product>
-}
+
+        suspend fun getLatestProduct(id: Int ): Response<Product>
+
+        suspend fun getLocalProduct (id: Int): Response<Product>
+    }
+   // suspend fun getLatestProduct(): Response<Product>
