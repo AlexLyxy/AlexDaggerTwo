f7afa0a6ccdc1d455a2139524f6d072c816c833e
Oleksandr Lysenko <alexlyxy@gmail.com>
2024-03-26 22:42:49 +0200

    Final fullProduct MyApp 26.03.2024

--- app/src/main/java/com/alexlyxy/alexretrofitlessontwo/presentation/MainActivity.kt
+++ app/src/main/java/com/alexlyxy/alexretrofitlessontwo/presentation/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity() {
 
     private val coroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
 
-    //private lateinit var productApi: ProductApi
+    private lateinit var productApi: ProductApi
 
     private var isDataLoaded = false
 
@@ -36,85 +36,71 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //getProductUseCase = GetProductUseCase(productApi)
+        getProductUseCase = GetProductUseCase(productApi)
 
         getProductUseCase = (application as MyApp).getProductUseCase
 
-        fetchProduct()
+        //fetchProduct()
+
 
         binding.button.setOnClickListener {
             CoroutineScope(Dispatchers.IO).launch {
                 val product = getProductUseCase.getLatestProduct()
 
                 Log.d("MyLog", "Product : $product")
+
+                runOnUiThread {
+
+                    binding.apply {
+
+                        tvTitle.text = buildString {
+                            append("Title:  ")
+                            append(product.body()?.title)
+                        }
+
+                        tvDescr.text = buildString {
+                            append("Description:  ")
+                            append(product.body()?.description)
+                        }
+                        tvPrice.text = buildString {
+                            append("Price:  ")
+                            append(product.body()?.price)
+                        }
+                        tvDiscount.text = buildString {
+                            append("DiscountPercentage:  ")
+                            append(product.body()?.discountPercentage)
+                        }
+                        tvRating.text = buildString {
+                            append("Rating:  ")
+                            append(product.body()?.rating)
+                        }
+                        tvStock.text = buildString {
+                            append("Stock:  ")
+                            append(product.body()?.stock)
+                        }
+                        tvBrand.text = buildString {
+                            append("Brand:  ")
+                            append(product.body()?.brand)
+                        }
+                        tvCategory.text = buildString {
+                            append("Category:  ")
+                            append(product.body()?.category)
+                        }
+                        tvThumbnail.text = buildString {
+                            append("Thumbnail:  ")
+                            append(product.body()?.thumbnail)
+                        }
+                        Picasso.get().load(product.body()!!.images[1]).into(ivImageOne)
+                        Picasso.get().load(product.body()!!.images[2]).into(ivImageTwo)
+                        Picasso.get().load(product.body()!!.images[3]).into(ivImageThree)
+
+                    }
+                }
             }
         }
-
-
-//                runOnUiThread {
-//
-//                    binding.apply {
-//
-//                        tvTitle.text = buildString {
-//                            append("Title:  ")
-//                            append(product.body()?.title)
-//                        }
-//
-//                        tvDescr.text = buildString {
-//                            append("Description:  ")
-//                            append(product.body()?.description)
-//                        }
-//                        tvPrice.text = buildString {
-//                            append("Price:  ")
-//                            append(product.body()?.price)
-//                        }
-//                        tvDiscount.text = buildString {
-//                            append("DiscountPercentage:  ")
-//                            append(product.body()?.discountPercentage)
-//                        }
-//                        tvRating.text = buildString {
-//                            append("Rating:  ")
-//                            append(product.body()?.rating)
-//                        }
-//                        tvStock.text = buildString {
-//                            append("Stock:  ")
-//                            append(product.body()?.stock)
-//                        }
-//                        tvBrand.text = buildString {
-//                            append("Brand:  ")
-//                            append(product.body()?.brand)
-//                        }
-//                        tvCategory.text = buildString {
-//                            append("Category:  ")
-//                            append(product.body()?.category)
-//                        }
-//                        tvThumbnail.text = buildString {
-//                            append("Thumbnail:  ")
-//                            append(product.body()?.thumbnail)
-//                        }
-//                        Picasso.get().load(product.body()!!.images[1]).into(ivImageOne)
-//                        Picasso.get().load(product.body()!!.images[2]).into(ivImageTwo)
-//                        Picasso.get().load(product.body()!!.images[3]).into(ivImageThree)
-        //                  }
-        //             }
-        //          }
-        //       }
-        //   }
-
-//    override fun onStart() {
-//        super.onStart()
-//        if (!isDataLoaded) {
-//            //fetchProduct()
-//            //fetchStart()
-//        }
-//    }
-//
-//    override fun onStop() {
-//        super.onStop()
-//        coroutineScope.coroutineContext.cancelChildren()
-//        //fetchStop()
-//    }
     }
+
+}
 
 
     //private fun fetchStart() {
@@ -127,88 +113,92 @@ class MainActivity : AppCompatActivity() {
 //        progressBarCircus.visibility = View.INVISIBLE
 //    }
 
-    private fun fetchProduct() {
+//    private fun fetchProduct() {
 //        val retrofit = Retrofit.Builder()
 //            .baseUrl(Constants.BASE_URL)
 //            .addConverterFactory(GsonConverterFactory.create())
 //            .build()
 //        productApi = retrofit.create(ProductApi::class.java)
-
-        coroutineScope.launch {
-
-            delay(1000)
-            try {
-//                //val response = getProductUseCase.getLatestProduct()
-                val response = getProductUseCase.getLocalProduct()
-
-                if (response.isSuccessful && response.body() != null) {
-                    isDataLoaded = true
-                    Log.d("MyLog", "Response : ${getProductUseCase.getLocalProduct()}")
-
-
 //
-//                    binding.apply {
+//        coroutineScope.launch {
 //
-//                        tvTitle.text = buildString {
-//                            append("Title:  ")
-//                            append(response.body()?.title)
+//            delay(1000)
+//            try {
+////                //val response = getProductUseCase.getLatestProduct()
+//                //val response = getProductUseCase.getLocalProduct()
+//                val response = productApi.getProduct(2)
+//
+//                if (response.isSuccessful && response.body() != null) {
+//                    isDataLoaded = true
+//                    Log.d("MyLog", "Response : $response")
+//
+//
+//                    runOnUiThread {
+//                        binding.apply {
+//
+//                            tvTitle.text = buildString {
+//                                append("Title:  ")
+//                                append(response.body()?.title)
+//                            }
+//                            tvDescr.text = buildString {
+//                                append("Description:  ")
+//                                append(response.body()?.description)
+//                            }
+//                            tvPrice.text = buildString {
+//                                append("Price:  ")
+//                                append(response.body()?.price)
+//                            }
+//                            tvDiscount.text = buildString {
+//                                append("DiscountPercentage:  ")
+//                                append(response.body()?.discountPercentage)
+//                            }
+//                            tvRating.text = buildString {
+//                                append("Rating:  ")
+//                                append(response.body()?.rating)
+//                            }
+//                            tvStock.text = buildString {
+//                                append("Stock:  ")
+//                                append(response.body()?.stock)
+//                            }
+//                            tvBrand.text = buildString {
+//                                append("Brand:  ")
+//                                append(response.body()?.brand)
+//                            }
+//                            tvCategory.text = buildString {
+//                                append("Category:  ")
+//                                append(response.body()?.category)
+//                            }
+//                            tvThumbnail.text = buildString {
+//                                append("Thumbnail:  ")
+//                                append(response.body()?.thumbnail)
+//                            }
+//                            Picasso.get().load(response.body()!!.images[1]).into(ivImageOne)
+//                            Picasso.get().load(response.body()!!.images[2]).into(ivImageTwo)
+//                            Picasso.get().load(response.body()!!.images[3]).into(ivImageThree)
 //                        }
-//                        tvDescr.text = buildString {
-//                            append("Description:  ")
-//                            append(response.body()?.description)
-//                        }
-//                        tvPrice.text = buildString {
-//                            append("Price:  ")
-//                            append(response.body()?.price)
-//                        }
-//                        tvDiscount.text = buildString {
-//                            append("DiscountPercentage:  ")
-//                            append(response.body()?.discountPercentage)
-//                        }
-//                        tvRating.text = buildString {
-//                            append("Rating:  ")
-//                            append(response.body()?.rating)
-//                        }
-//                        tvStock.text = buildString {
-//                            append("Stock:  ")
-//                            append(response.body()?.stock)
-//                        }
-//                        tvBrand.text = buildString {
-//                            append("Brand:  ")
-//                            append(response.body()?.brand)
-//                        }
-//                        tvCategory.text = buildString {
-//                            append("Category:  ")
-//                            append(response.body()?.category)
-//                        }
-//                        tvThumbnail.text = buildString {
-//                            append("Thumbnail:  ")
-//                            append(response.body()?.thumbnail)
-//                        }
-//                        Picasso.get().load(response.body()!!.images[1]).into(ivImageOne)
-//                        Picasso.get().load(response.body()!!.images[2]).into(ivImageTwo)
-//                        Picasso.get().load(response.body()!!.images[3]).into(ivImageThree)
+//                    }
+//
+//                } else {
+//                    //onFetchFailed()
+//                }
+//            } catch (t: Throwable) {
+//                if (t !is CancellationException) {
+//                    onFetchFailed()
+//                }
+//            } finally {
+//                // fetchStop()
+//            }
+//        }
+//    }
+//
+//    private fun onFetchFailed() {
+//        supportFragmentManager.beginTransaction()
+//            .add(ServerErrorDialogFragment.newInstance(), null)
+//            .commitAllowingStateLoss()
+//    }
+//}
 
-                } else {
-                    //onFetchFailed()
-                }
-            } catch (t: Throwable) {
-                if (t !is CancellationException) {
-                    // onFetchFailed()
-                }
-            } finally {
-                // fetchStop()
-            }
-        }
-    }
 
-
-    private fun onFetchFailed() {
-        supportFragmentManager.beginTransaction()
-            .add(ServerErrorDialogFragment.newInstance(), null)
-            .commitAllowingStateLoss()
-    }
-}
 
 
 
