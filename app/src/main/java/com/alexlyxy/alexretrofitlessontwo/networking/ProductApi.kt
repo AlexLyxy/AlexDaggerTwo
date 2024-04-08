326660735637b80ae5250c426167f2a6a8bdb718
Oleksandr Lysenko <alexlyxy@gmail.com>
2024-04-09 01:50:12 +0300

    Fetch Product & Products 08/04/2024

--- app/src/main/java/com/alexlyxy/alexretrofitlessontwo/networking/ProductApi.kt
+++ app/src/main/java/com/alexlyxy/alexretrofitlessontwo/networking/ProductApi.kt
@@ -1,8 +1,7 @@
 package com.alexlyxy.alexretrofitlessontwo.networking
 
-import com.alexlyxy.alexretrofitlessontwo.products.Product
 import com.alexlyxy.alexretrofitlessontwo.products.AllProduct
-import com.alexlyxy.alexretrofitlessontwo.products.ProductView
+import com.alexlyxy.alexretrofitlessontwo.products.Product
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
--- app/src/main/java/com/alexlyxy/alexretrofitlessontwo/products/ProductView.kt
+++
@@ -1,7 +0,0 @@
-package com.alexlyxy.alexretrofitlessontwo.products
-
-data class ProductView (
-    val id: Int,
-    var title: String,
-    val images: List<String>
-)
