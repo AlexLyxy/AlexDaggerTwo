9804283495b7046b60514dd68f6daa787dfbaf09
Oleksandr Lysenko <alexlyxy@gmail.com>
2024-10-30 21:58:08 +0200

    Branch Fetch Product Simple  Revert ScreensNavigator to  ActivityModule for manage in the same place 30/10/2024

--- app/src/main/java/com/alexlyxy/alexretrofitlessontwo/commonApp/dependencyinjection/activity/ActivityModule.kt
+++ app/src/main/java/com/alexlyxy/alexretrofitlessontwo/commonApp/dependencyinjection/activity/ActivityModule.kt
@@ -2,6 +2,7 @@ package com.alexlyxy.alexretrofitlessontwo.commonApp.dependencyinjection.activit
 
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import com.alexlyxy.alexretrofitlessontwo.screens.commonScreens.ScreensNavigator
 import dagger.Module
 import dagger.Provides
 
@@ -12,6 +13,10 @@ class ActivityModule(
 
     @Provides
     fun activity() = activity
+
+    @Provides
+    @ActivityScope
+    fun screensNavigator(activity: AppCompatActivity) = ScreensNavigator(activity)
 
     @Provides
     fun layoutInflater(activity: AppCompatActivity) = LayoutInflater.from(activity)
--- app/src/main/java/com/alexlyxy/alexretrofitlessontwo/screens/commonScreens/ScreensNavigator.kt
+++ app/src/main/java/com/alexlyxy/alexretrofitlessontwo/screens/commonScreens/ScreensNavigator.kt
@@ -1,13 +1,12 @@
 package com.alexlyxy.alexretrofitlessontwo.screens.commonScreens
 
-import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import com.alexlyxy.alexretrofitlessontwo.commonApp.dependencyinjection.activity.ActivityScope
 import com.alexlyxy.alexretrofitlessontwo.screens.productdetails.DetailsActivity
 import javax.inject.Inject
 
 @ActivityScope
-class ScreensNavigator @Inject constructor (private val activity: AppCompatActivity){
+class ScreensNavigator  (private val activity: AppCompatActivity){
 
     fun navigateBack() {
         activity.onBackPressed()
