0eef3af02750906f745d7c91735cada04fae16bc
Oleksandr Lysenko <alexlyxy@gmail.com>
2024-12-22 18:08:35 +0200

    Alex Dagger Two MinimumFinal 22/12/2024

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.alexlyxy.alexdaggertwo"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.alexlyxy.alexdaggertwo"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -45,9 +45,7 @@ android {
 dependencies {
 
     implementation("com.google.dagger:dagger:2.47")
-    //annotationProcessor("com.google.dagger:dagger-compiler:2.47")
     kapt ("com.google.dagger:dagger-compiler:2.47")
-
 
     implementation("com.squareup.okhttp3:logging-interceptor:4.7.2")
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
@@ -55,18 +53,16 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
-    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
     implementation("com.squareup.picasso:picasso:2.71828")
-
-    implementation( "com.github.bumptech.glide:glide:4.16.0@")
 
     implementation ("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
